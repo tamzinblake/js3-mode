tamzinblake/js3-mode

commit 66451ad50506faa09ebf85df410d34f4e2cf7bce

Adds lazy mode config options
closes issue #8, issue #9, issue #10

js.el
@@ -506,6 +506,24 @@ getting timeout messages."
   :type 'integer
   :group 'js)
 
+(defcustom js-lazy-commas nil
+  "Whether `js-mode' should line up commas to the indent-minus-2,
+rather than trying to line up to braces."
+  :type 'boolean
+  :group 'js)
+
+(defcustom js-lazy-operators nil
+  "Whether `js-mode' should line up operators to the indent-minus-2,
+rather than trying to line up to braces."
+  :type 'boolean
+  :group 'js)
+
+(defcustom js-lazy-dots nil
+  "Whether `js-mode' should line up dots to the next indent level,
+rather than trying to line up to dots."
+  :type 'boolean
+  :group 'js)
+
 ;;; KeyMap
 
 (defvar js-mode-map
@@ -1842,7 +1860,8 @@ nil."
     (cond
 
      ;;comma-first
-     ((= (following-char) ?\,)
+     ((and (not js-lazy-commas)
+	   (= (following-char) ?\,))
       (let ((spos
              (save-excursion
 	       (while (looking-back (concat "[]})\"']" js--skip-newlines-re))
@@ -1872,8 +1891,29 @@ nil."
             spos
           (+ js-indent-level js-expr-indent-offset))))
 
+     ;;dot-first
+     ((and (not js-lazy-dots)
+	   (= (following-char) ?\.))
+      (save-excursion
+        (when (looking-back (concat "[]})\"']" js--skip-newlines-re))
+          (js--backward-sexp)
+	  (js--backward-sexp-strings))
+        (if (not (looking-back (concat "^[ \t]*\\([]})]+\\|.*\\..*\\)"
+				       js--skip-newlines-re)))
+            (progn
+              (re-search-backward concat("\\<[^ \t]+" js--skip-newlines-re)
+				  (point-min) t)
+              (re-search-backward "^" (point-min) t)
+              (back-to-indentation)
+              (+ (current-column) js-indent-level))
+          (progn
+            (re-search-backward concat("\\..*" js--skip-newlines-re)
+				(point-min) t)
+            (current-column)))))
+
      ;;operator-first
-     ((looking-at js--indent-operator-re)
+     ((and (not js-lazy-operators)
+	   (looking-at js--indent-operator-re))
       (let ((spos nil))
 	(save-excursion
 	  (while (looking-back (concat "[]})\"']" js--skip-newlines-re))
@@ -1918,24 +1958,55 @@ nil."
 	      spos
 	    (+ js-indent-level js-expr-indent-offset)))))
 
-     ;;dot-first
-     ((= (following-char) ?\.)
+     ;;lazy comma-first
+     ((and js-lazy-commas
+	   (= (following-char) ?\,))
       (save-excursion
-        (when (looking-back (concat "[]})\"']" js--skip-newlines-re))
-          (js--backward-sexp)
-	  (js--backward-sexp-strings))
-        (if (not (looking-back (concat "^[ \t]*\\([]})]+\\|.*\\..*\\)"
-				       js--skip-newlines-re)))
-            (progn
-              (re-search-backward concat("\\<[^ \t]+" js--skip-newlines-re)
+	(js--backward-sexp)
+	(cond
+
+	 ((looking-back (concat "^[ \t]*,.*" js--skip-newlines-re))
+	  (re-search-backward (concat "^[ \t],.*" js--skip-newlines-re)
+			      (point-min) t)
+	  (back-to-indentation)
+	  (current-column))
+
+	 ((looking-back (concat "^[ \t]*[^ \t\n].*" js--skip-newlines-re))
+	  (re-search-backward (concat "^[ \t]*[^ \t\n].*" js--skip-newlines-re)
+			      (point-min) t)
+	  (back-to-indentation)
+	  (- (current-column) 2))
+
+	 (t
+	  (+ js-indent-level js-expr-indent-offset)))))
+
+     ;;lazy dot-first
+     ((and js-lazy-dots
+	   (= (following-char) ?\.))
+      (save-excursion
+	(js--backward-sexp)
+	(if (looking-back (concat "^[ \t]*[^ \t\n].*" js--skip-newlines-re))
+	    (progn
+	      (re-search-backward (concat "^[ \t]*[^ \t\n].*"
+					  js--skip-newlines-re)
 				  (point-min) t)
-              (re-search-backward "^" (point-min) t)
-              (back-to-indentation)
-              (+ (current-column) js-indent-level))
-          (progn
-            (re-search-backward concat("\\..*" js--skip-newlines-re)
-				(point-min) t)
-            (current-column)))))
+	      (back-to-indentation)
+	      (+ (current-column) js-indent-level))
+	  (+ js-indent-level js-expr-indent-offset))))
+
+     ;;lazy operator-first
+     ((and js-lazy-operators
+	   (looking-at js--indent-operator-re))
+      (save-excursion
+	(js--backward-sexp)
+	(if (looking-back (concat "^[ \t]*[^ \t\n].*" js--skip-newlines-re))
+	    (progn
+	      (re-search-backward (concat "^[ \t]*[^ \t\n].*"
+					  js--skip-newlines-re)
+				  (point-min) t)
+	      (back-to-indentation)
+	      (- (current-column) 2))
+	  (+ js-indent-level js-expr-indent-offset))))
 
      ;;var special case for non-comma-first continued var statements
      ((and (looking-at "[^]})]")

alternate_styles.js
@@ -1,0 +1,117 @@
+//This file describes the js-lazy-commas, js-lazy-operators, and
+// js-lazy-dots config options.
+//It is recommended that if you use js-lazy-commas or js-lazy-operators,
+// then you should use both, due to the funky indentation it causes otherwise
+
+/*
+
+// To turn on all 3, add the following to your .emacs file:
+
+(custom-set-variables
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(js-lazy-commas t)
+ '(js-lazy-operators t)
+ '(js-lazy-dots t)
+ '(js-expr-indent-offset 2)
+ '(js-paren-indent-offset 2)
+ '(js-square-indent-offset 2)
+ '(js-curly-indent-offset 2))
+*/
+
+/*
+*   js-lazy-commas
+*/
+
+//By default, js-mode supports the following comma-first style:
+
+var obj1 = { prop1: { prop1: "val1"
+                    , prop2: "val2"
+                    }
+           , prop2: "val3"
+           }
+
+//By turning on js-lazy-commas with the recommended settings, you can use
+// the following style:
+
+var obj2 = {
+    prop1: {
+        prop1: "val1"
+      , prop2: "val2"
+    }
+  , prop2: "val3"
+}
+
+//Turn this on by putting the following in your .emacs file:
+/*
+
+(custom-set-variables
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(js-lazy-commas t)
+ '(js-expr-indent-offset 2)
+ '(js-paren-indent-offset 2)
+ '(js-square-indent-offset 2)
+ '(js-curly-indent-offset 2))
+*/
+
+/*
+*   js-lazy-operators
+*/
+
+//By default, js-mode supports the following operator-first style:
+
+var result1 = ( 5
+              + 7
+              / 2
+              * 5
+              )
+
+//By turning on js-lazy-operators with the recommended settings, you can use
+// the following style:
+
+var result2 = (
+    5
+  + 7
+  / 2
+  * 5
+)
+
+//Turn this on by putting the following in your .emacs file:
+/*
+
+(custom-set-variables
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(js-lazy-operators t)
+ '(js-expr-indent-offset 2)
+ '(js-paren-indent-offset 2)
+ '(js-square-indent-offset 2)
+ '(js-curly-indent-offset 2))
+*/
+
+/*
+*   js-lazy-dots
+*/
+
+//By default, js-mode supports the following dot-first style:
+//(indent to the previous dot)
+
+foo.bar()
+   .baz()
+
+//By turning on js-lazy-dots, you can use the following style:
+//(indent once)
+
+foo.bar()
+  .baz()
+
+//Turn this on by putting the following in your .emacs file:
+/*
+
+(custom-set-variables
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(js-lazy-dots t)
+*/
+

commit e626d3a26ccd614e1bf4557617cb9ecc3a4d455d

fix bug introduced in commit 66451ad5
Call the concat function correctly, rather than pretend I'm writing JS

js.el
@@ -1901,13 +1901,13 @@ nil."
         (if (not (looking-back (concat "^[ \t]*\\([]})]+\\|.*\\..*\\)"
 				       js--skip-newlines-re)))
             (progn
-              (re-search-backward concat("\\<[^ \t]+" js--skip-newlines-re)
+              (re-search-backward (concat "\\<[^ \t]+" js--skip-newlines-re)
 				  (point-min) t)
               (re-search-backward "^" (point-min) t)
               (back-to-indentation)
               (+ (current-column) js-indent-level))
           (progn
-            (re-search-backward concat("\\..*" js--skip-newlines-re)
+            (re-search-backward (concat "\\..*" js--skip-newlines-re)
 				(point-min) t)
             (current-column)))))
 