ca44b56789c63dc7009fd0442e7c9686347ea6e4
Thom Blake <thom@odonnellpdc.com>
2012-03-15 12:23:09 -0400

    Don't clobber minibuffer contents with warnings
    Apply https://github.com/mooz/js2-mode/commit/580ad93e26221610e2b871bca1bd6fdd6e87ec2e
    
    close #47

--- lib/js3-foot.el
+++ lib/js3-foot.el
@@ -363,7 +363,8 @@ This ensures that the counts and `next-error' are correct."
 (defun js3-echo-error (old-point new-point)
   "Called by point-motion hooks."
   (let ((msg (get-text-property new-point 'help-echo)))
-    (if msg
+    (if (and msg (or (not (current-message))
+		     (string= (current-message) "Quit")))
         (message msg))))
 
 (defalias #'js3-echo-help #'js3-echo-error)
